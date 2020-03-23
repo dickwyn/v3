24fa4ce88a2276af1ceb19c8536fbcbd4d47a19b
dickwyn <dickwyn@outlook.com>
2020-03-22 22:47:49 -0700

    (feat) Convert css to scss and implement holy grail layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "../scss/layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -27,6 +27,7 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
+        className="grid"
         style={{
           margin: `0 auto`,
           maxWidth: 960,
