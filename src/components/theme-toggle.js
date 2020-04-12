f3431a1cde9773ba93f4830d408b2fde1270b921
dickwyn <dickwyn@outlook.com>
2020-04-11 23:52:29 -0700

    (feat) Add auto state for theme toggle component

--- src/components/theme-toggle.js
+++ src/components/theme-toggle.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 import { FiSun, FiMoon } from 'react-icons/fi';
 import '../scss/components/theme-toggle.scss';
 
-const ThemeToggle = () => (
-  <ThemeToggler>
-    {({ theme, toggleTheme }) => (
-      <button
-        type="button"
-        onClick={() => toggleTheme(theme === 'light' ? 'dark' : 'light')}
-      >
-        {theme === 'dark' ? (
-          <FiMoon className="lightLogo" />
-        ) : (
-          <FiSun className="darkLogo" />
+class ThemeToggle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 0,
+    };
+  }
+
+  handleToggle = (theme, toggleTheme) => {
+    const { mode } = this.state;
+    this.setState(prevState => ({
+      mode: (prevState.mode + 1) % 3,
+    }));
+    if (mode !== 2) {
+      toggleTheme(theme === 'light' ? 'dark' : 'light');
+    }
+  };
+
+  render() {
+    return (
+      <ThemeToggler>
+        {({ theme, toggleTheme }) => (
+          <button type="button" onClick={() => this.handleToggle(theme, toggleTheme)}>
+            {theme === 'light' ? <FiMoon className="lightLogo" /> : <FiSun className="darkLogo" />}
+          </button>
         )}
-      </button>
-    )}
-  </ThemeToggler>
-);
+      </ThemeToggler>
+    );
+  }
+}
 
 export default ThemeToggle;
