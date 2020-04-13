5ba9c63463788e7977c930d5cd00af5bb2f51a47
dickwyn <dickwyn@outlook.com>
2020-04-12 23:14:28 -0700

    (style) Implement flexbox for Hero component

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,20 +4,26 @@ import Image from './image';
 const Hero = () => (
   <div className="hero-container">
     <div className="content-container">
-      <Image />
-      <h1>Dick Wyn Yong</h1>
-      <h2>Software Engineer in Phoenix</h2>
-      <p>
-        I am a recent Computer Science graduate from Arizona State University. I have a strong
-        passion for web development, mobile computing and human-computer interaction. I also enjoy
-        writing blogs, taking photos and travelling.
-      </p>
-      <p>
-        Feel free to send me a message on
-        <a href="https://www.twitter.com/dickwyn" target="_blank" rel="noopener noreferrer">
-          Twitter
-        </a>
-      </p>
+      <div className="profile-card">
+        <div className="title">
+          <h1>Dick Wyn Yong</h1>
+          <h2>Software Engineer in Phoenix</h2>
+        </div>
+        <Image />
+      </div>
+      <div className="description">
+        <p>
+          I am a recent Computer Science graduate from Arizona State University. I have a strong
+          passion for web development, mobile computing and human-computer interaction. I also enjoy
+          writing blogs, taking photos and travelling.
+        </p>
+        <p>
+          Feel free to send me a message on
+          <a href="https://www.twitter.com/dickwyn" target="_blank" rel="noopener noreferrer">
+            Twitter
+          </a>
+        </p>
+      </div>
     </div>
   </div>
 );
