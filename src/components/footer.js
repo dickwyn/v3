0c9c17951d4355ef1df725785afa9e8f105f5fe0
dickwyn <dickwyn@outlook.com>
2020-04-18 10:46:13 -0700

    (feat) Use material icons for footer social links

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
+import IconButton from '@material-ui/core/IconButton';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import EmailIcon from '@material-ui/icons/Email';
 
 // eslint-disable-next-line no-unused-vars
 const SOCIAL_LINKS = [
@@ -9,26 +16,37 @@ const SOCIAL_LINKS = [
     id: shortid.generate(),
     name: 'Twitter',
     url: 'https://twitter.com/dickwyn',
+    component: <TwitterIcon />,
   },
   {
     id: shortid.generate(),
     name: 'Github',
-    url: 'https://twitter.com/dickwyn',
+    url: 'https://github.com/dickwyn',
+    component: <GitHubIcon />,
   },
   {
     id: shortid.generate(),
     name: 'LinkedIn',
-    url: 'https://twitter.com/dickwyn',
+    url: 'https://linkedin.com/in/dickwyn',
+    component: <LinkedInIcon />,
   },
   {
     id: shortid.generate(),
     name: 'YouTube',
-    url: 'https://twitter.com/dickwyn',
+    url: 'https://youtube.com/dickwyn',
+    component: <YouTubeIcon />,
   },
   {
     id: shortid.generate(),
     name: 'Instagram',
-    url: 'https://twitter.com/dickwyn',
+    url: 'https://instagram.com/dickwyn',
+    component: <InstagramIcon />,
+  },
+  {
+    id: shortid.generate(),
+    name: 'Email',
+    url: 'mailto:dickwyn@outlook.com',
+    component: <EmailIcon />,
   },
 ];
 
@@ -38,10 +56,18 @@ const Footer = ({ hide }) => (
       <div className="content-container">
         <div className="left">© {new Date().getFullYear()} Dick Wyn Yong</div>
         <div className="right">
-          <a href="https://instagram.com/dickwyn">Instagram</a>
-          <a href="https://twitter.com/dickwyn">Twitter</a>
-          <Link to="/">Blog</Link>
-          <a href="mailto:dickwyn@outlook.com">Contact</a>
+          {SOCIAL_LINKS.map(item => (
+            <IconButton
+              key={item.id}
+              size="small"
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label={`${item.name} profile`}
+            >
+              {item.component}
+            </IconButton>
+          ))}
           <Link to="/privacy">Privacy</Link>
         </div>
       </div>
