5ae2e074abe98a46faf5eae4cb6a5248ea4ad6cf
dickwyn <dickwyn@outlook.com>
2020-04-20 21:37:42 -0700

    (feat) Move console credits to gatsby-ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,43 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
+import Terser from 'terser';
+import preval from 'preval.macro';
+import PackageJson from './package.json';
 
-// You can delete this file if you're not using it
+const buildTimestamp = preval`module.exports = new Date().getTime();`;
+
+function initialize() {
+  const nodeEnv = 'unsetEnv';
+
+  if (nodeEnv === 'production') {
+    console.log(
+      `%cDick Wyn Yong's Portfolio vAPP_VERSION
+              
+Looking for something?
+
+👨‍💻 https://www.github.com/dickwyn/v5
+📄 https://www.dropbox.com/s/n1qj7bfp9599lg2/resume-dick-wyn-yong.pdf?dl=0
+📧 dickwyn@outlook.com
+
+built: unsetTimestamp`,
+      'font-family:monospace;'
+    );
+  }
+}
+
+const InitializeScript = () => {
+  const boundFn = String(initialize)
+    .replace('APP_VERSION', PackageJson.version)
+    .replace('unsetEnv', process.env.NODE_ENV)
+    .replace('unsetTimestamp', new Date(buildTimestamp).toString());
+
+  let calledFunction = `(${boundFn})()`;
+
+  calledFunction = Terser.minify(calledFunction).code;
+
+  // eslint-disable-next-line react/no-danger
+  return <script dangerouslySetInnerHTML={{ __html: calledFunction }} />;
+};
+
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents(<InitializeScript />);
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,6 @@ import SEO from './seo';
 import Header from './header';
 import Footer from './footer';
 import '../scss/main.scss';
-import PackageJson from '../../package.json';
 
 const Layout = ({
   page,
@@ -22,18 +21,6 @@ const Layout = ({
       <Header hide={hideHeader} invisible={invisible} />
       <main className={className}>{children}</main>
       <Footer hide={hideFooter} />
-      {process.browser &&
-        process.env.NODE_ENV === 'production' &&
-        console.log(
-          `%cDick Wyn Yong's Portfolio v${PackageJson.version}
-
-Looking for something?
-
-👨‍💻 https://www.github.com/dickwyn/v5
-📄 https://www.dropbox.com/s/n1qj7bfp9599lg2/resume-dick-wyn-yong.pdf?dl=0
-📧 dickwyn@outlook.com`,
-          'font-family:monospace;font-size:12px;'
-        )}
     </div>
   );
 };
