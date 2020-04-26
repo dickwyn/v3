2f717ebfdf641d95b488b97dba86d292c746dbfc
dickwyn <dickwyn@outlook.com>
2020-04-25 23:41:29 -0700

    (feat) Create experiments page

---
+++ src/pages/experiments.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const ExperimentsPage = () => {
+  return (
+    <Layout page="Experiments">
+      <div className="wrapper">
+        <div className="content-container with-margin">
+          <h1>Experiments</h1>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default ExperimentsPage;
