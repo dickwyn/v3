ff61f178afd0e0849b12c3c79b3e625768ff4ae9
dickwyn <dickwyn@outlook.com>
2020-04-26 00:38:44 -0700

    (feat) Update look and feel of header home link

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,9 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
 import DarkModeToggle from './dark-mode-toggle';
+import { LOGO } from '../utils/constants';
 
 const NAV_LINKS = [
-  {
-    id: shortid.generate(),
-    name: 'dickwyn',
-    path: '/',
-  },
   {
     id: shortid.generate(),
     name: 'blog',
@@ -23,9 +19,14 @@ const NAV_LINKS = [
 ];
 
 const Header = ({ hide, invisible }) => (
-  <header className={hide ? 'hide' : undefined}>
-    <div className={`content-container ${invisible ? 'invisible' : undefined}`}>
+  <header className={`${hide ? 'hide' : undefined} ${invisible ? 'invisible' : undefined}`}>
+    <div className={'content-container'}>
       <nav role="navigation">
+        <Link to="/" activeClassName="active" className="home">
+          <img src={LOGO}></img>
+          <p className="name">dickwyn</p>
+          <p className="short-name">dw</p>
+        </Link>
         {NAV_LINKS.map(item => (
           <React.Fragment key={item.id}>
             <Link to={item.path} activeClassName="active">
