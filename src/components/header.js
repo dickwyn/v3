1a7370f5af7fb376f5194b5f5d1a7d3ebf2718b8
Dick Wyn Yong <11251672+dickwyn@users.noreply.github.com>
2020-06-21 17:58:19 -0700

    (feat) Implement hamburger menu for mobile viewport (#1)
    * (feat) initial implementation of hamburger menu
    
    * (feat) Update hamburger button to be accessible
    
    * (chore) Cleanup styling of hamburger menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
 import DarkModeToggle from './dark-mode-toggle';
+import HamburgerToggle from './hamburger-toggle';
 
 const NAV_LINKS = [
   {
@@ -17,25 +18,47 @@ const NAV_LINKS = [
   },
 ];
 
-const Header = ({ hide, invisible }) => (
-  <header className={`${hide ? 'hide' : undefined} ${invisible ? 'invisible' : undefined}`}>
-    <div className={'content-container'}>
-      <nav role="navigation">
-        <Link to="/" activeClassName="active" className="home">
-          <p className="name">dickwyn</p>
+const SiteLinks = () => (
+  <>
+    {NAV_LINKS.map((item) => (
+      <React.Fragment key={item.id}>
+        <Link to={item.path} activeClassName="active" partiallyActive={true}>
+          {item.name}
         </Link>
-        {NAV_LINKS.map((item) => (
-          <React.Fragment key={item.id}>
-            <Link to={item.path} activeClassName="active" partiallyActive={true}>
-              {item.name}
-            </Link>
-          </React.Fragment>
-        ))}
-      </nav>
-      <DarkModeToggle />
-    </div>
-  </header>
+      </React.Fragment>
+    ))}
+  </>
 );
+
+const Header = ({ hide, invisible }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className={`${hide ? 'hide' : undefined} ${invisible ? 'invisible' : undefined}`}>
+      <div className="common-nav">
+        <div className="content-container">
+          <nav role="navigation">
+            <Link to="/" activeClassName="active" className="home">
+              <p className="name">dickwyn</p>
+            </Link>
+            <div className={`desktop-links`}>
+              <SiteLinks />
+            </div>
+          </nav>
+          <div className="action-buttons">
+            <DarkModeToggle />
+            <HamburgerToggle open={open} setOpen={setOpen} />
+          </div>
+        </div>
+      </div>
+      <div className={`mobile-nav ${open ? undefined : 'hide'}`}>
+        <div className="content-container">
+          <SiteLinks />
+        </div>
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   hide: PropTypes.bool.isRequired,
