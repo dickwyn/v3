4f72b9f0a05429fea67bd2830aaf2a71102181d1
Dick Wyn Yong <11251672+dickwyn@users.noreply.github.com>
2020-10-31 23:38:18 -0700

    (chore) Display line number for code block (#15)

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,7 @@ module.exports = {
               classPrefix: 'language-',
               inlineCodeMarker: null,
               aliases: {},
-              showLineNumbers: false,
+              showLineNumbers: true,
               noInlineHighlight: false,
               languageExtensions: [
                 {
