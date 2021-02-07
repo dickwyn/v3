4828d15cb34bf003d3e11f608d1124699d18d08e
Dick Wyn Yong <dickwyn@outlook.com>
2021-02-06 22:46:04 -0700

    (chore) create youtube setup section for uses screen

--- data/uses.js
+++ data/uses.js
@@ -84,7 +84,10 @@ export const uses = [
       {
         name: 'Roav Sunglasses',
         url: 'https://roaveyewear.com/collections/origin-series/products/freddy-black-moonrise',
-        attributeList: [{ name: 'I have the Freddy frame/shape in Black/Sunrise colorway' }, { name: 'These are durable, lightweight and folds into my pocket.' }],
+        attributeList: [
+          { name: 'I have the Freddy frame/shape in Black/Sunrise colorway' },
+          { name: 'These are durable, lightweight and folds into my pocket.' },
+        ],
       },
     ],
   },
@@ -94,7 +97,7 @@ export const uses = [
       'Since I spend a lot of time creating content, I have invested in quite a bit of gear to help streamline my workflow.',
     categoryList: [
       {
-        name: 'setup',
+        name: 'desk setup',
         list: [
           {
             name: 'MacBook Pro 16-inch (Late 2019)',
@@ -126,23 +129,63 @@ export const uses = [
             url: 'https://happyhackingkb.com/',
           },
           { name: 'LTT Northern Lights Desk Pad (1000mm x 600mm)' },
-          { name: 'DaVinci Resolve Speed Editor' },
+          // { name: 'DaVinci Resolve Speed Editor' },
+        ],
+      },
+      {
+        name: 'youtube setup',
+        list: [
+          {
+            name: 'Panasonic Lumix DC-G9',
+            attributeList: [
+              { name: 'Sigma 18-35mm f/1.8 for Canon EF' },
+              { name: 'Viltrox EF-M2 II' },
+              { name: '128GB SanDisk Extreme PRO SD Card' },
+              { name: 'Manfrotto 200PL Quick Release Plate' },
+            ],
+          },
+          {
+            name: 'Sound Devices MixPre-3 II',
+            attributeList: [
+              { name: '64GB SanDisk Extreme SD Card' },
+              { name: 'Rode VideoMic NTG' },
+              { name: 'Sony MDR-7506' },
+              { name: 'Anker PowerCore Slim 10000 PD' },
+            ],
+          },
+          {
+            name: 'Manfrotto Element Traveller Tripod',
+            attributeList: [
+              { name: 'Manfrotto XPRO Fluid Head' },
+              { name: 'Tri-Wheel Leveling Base with Bubble Level' },
+            ],
+          },
+          {
+            name: 'Godox VL150 LED Video Light',
+            attributeList: [
+              { name: 'I use this as my main key light' },
+              { name: 'Glow EZ Lock Deep Parabolic Quick Softbox (28")' },
+            ],
+          },
+          {
+            name: 'Aputure MC RGBWW LED Light',
+          },
+          {
+            name: 'GVM 480LS Bi-color LED Light',
+          },
+          { name: 'Elvid 9-Section Acrylic Production Slate with Color Clapper Sticks' },
+          { name: 'X-Rite ColorChecker Passport Video' },
         ],
       },
       {
         name: 'storage',
         sort: true,
-        list: [
-          { name: 'Samsung T5 SSD' },
-          { name: 'Sandisk Extreme PRO SD Card' },
-          { name: 'Sandisk Extreme micro SD Card' },
-          { name: 'Western Digital My Passport Ultra' },
-        ],
+        list: [{ name: '500GB Samsung T5 SSD' }, { name: '5TB Western Digital My Passport Ultra' }],
       },
       {
         name: 'camera',
         sort: true,
-        list: [{ name: 'Canon AE-1 Program' }, { name: 'Panasonic Lumix DC-G9' }],
+        list: [{ name: 'Canon AE-1 Program' }],
       },
       {
         name: 'lenses',
@@ -152,49 +195,27 @@ export const uses = [
           { name: 'Helios 44-2 58mm f/2.0' },
           { name: 'Olympus 12-40mm f/2.8 PRO' },
           { name: 'Panasonic 7-14mm f/4.0' },
-          { name: 'Panasonic 25mm f/1.7' },
-          { name: 'Sigma 18-35mm f/1.8 for Canon EF' },
         ],
       },
       {
         name: 'audio',
         sort: true,
-        list: [
-          { name: 'Rode VideoMic NTG' },
-          { name: 'Rode VideoMicro' },
-          { name: 'Sony MDR-7506' },
-          { name: 'Tascam DR-10L' },
-          { name: 'Zoom H1' },
-        ],
+        list: [{ name: 'Rode VideoMicro' }, { name: 'Tascam DR-10L' }, { name: 'Zoom H1' }],
       },
       {
         name: 'camera accessories',
         sort: true,
         list: [
           { name: 'Giottos Rocket Air Blaster' },
-          { name: 'Manfrotto Element Traveller Tripod' },
-          { name: 'Manfrotto XPRO Fluid Head' },
-          { name: 'Parrot Teleprompter 2' },
           { name: 'Peak Design Slide Lite' },
           { name: 'Pelican SD Memory Card Case' },
-          {
-            name: 'Viltrox EF-M2 II',
-            attributeList: [{ name: 'Used to adapt Canon EF lenses to MFT.' }],
-          },
-          { name: 'Aputure MC RGBWW LED Light' },
-          { name: 'Godox VL150 LED Video Light' },
           { name: 'Neewer 43-inch Light Reflector 5-in-1' },
-          {
-            name: 'Glow EZ Lock Deep Parabolic Quick Softbox (28")',
-            attributeList: [{ name: 'Bowens Mount modifier for the Godox VL150' }],
-          },
-          { name: 'Elvid 9-Section Acrylic Production Slate with Color Clapper Sticks' },
           { name: 'Peak Design Camera Cube' },
           { name: 'Eneloop AA Rechargeable Battery' },
           { name: 'Amazon Basics AAA Rechargeable Battery' },
           { name: 'Rycote Undercover with Stickies' },
           { name: 'Bongo Ties' },
-          { name: 'Peak Design Dual Plate V2' },
+          { name: 'Robo-Grip Pliers' },
         ],
       },
     ],
@@ -222,6 +243,7 @@ export const uses = [
           { name: 'Logi Options' },
           { name: 'Audacity' },
           { name: 'Microsoft To Do' },
+          { name: 'Microsoft Edge' },
         ],
       },
       {
@@ -230,7 +252,6 @@ export const uses = [
         list: [
           { name: '1Password 7' },
           { name: 'Alfred 4' },
-          { name: 'Dozer' },
           { name: 'iTerm' },
           { name: 'MacTeX' },
           { name: 'Magnet' },
@@ -269,7 +290,6 @@ export const uses = [
           { name: 'Grammarly' },
           { name: 'HTTPS Everywhere' },
           { name: 'META SEO inspector' },
-          { name: 'Privacy Badger' },
           { name: 'React Developer Tools' },
           { name: 'uBlock Origin' },
           { name: 'vidIQ Vision for YouTube' },
@@ -279,6 +299,7 @@ export const uses = [
           { name: 'Disconnect' },
           { name: 'Save to Pocket' },
           { name: 'The Camelizer' },
+          { name: 'ClearURLs' },
         ],
       },
       {
